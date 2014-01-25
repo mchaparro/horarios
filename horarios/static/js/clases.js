bdbcc18644edd8302350c0b7297b9c0fce732d8b
mchaparro <mx.mik3@gmail.com>
2014-01-25 03:14:56 -0700

    added loading gif

--- horarios/static/js/clases.js
+++ horarios/static/js/clases.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
 	load_clases(fecha);
 	$('.week-day-picker td').click(function(){
+		
 		$('.week-day-picker td').removeClass('activo');
-		$(this).toggleClass('activo');
+		
 											
 	});
 	
-	salon = "BIGROOM";
 	$("tr:odd").addClass("odd");
 	
 	$('.escoger-fecha').click(function(){
 		fecha = $(this).attr('data-fecha');
+		$(this).toggleClass('activo');
+		$('*[data-id="saloncito"]').remove();
+		load_clases(fecha);
 	});
 	
 		
--- horarios/static/js/load_clases.js
+++ horarios/static/js/load_clases.js
@@ -4,16 +4,17 @@ function load_clases(fecha){
 			url: '/get/clases/',
 			dataType: 'json',
 			beforeSend: function() {
-			    // $('#loading-gif').show();
+			    $('#loading').show();
 			},
 		    complete: function(){
-			     //$('#loading-gif').hide();
+			    $('#loading').hide();
 			},
 			data: { 'fecha':fecha },
 			success: function(response) {
+				$('*[data-id="saloncito"]').remove();
 				$.each(response, function(){
 				var td = $('.grupo-'+this.grupo+'-hora-'+this.hora);
-				td.append("<div data-id='saloncito' class='" +this.salon+ "'>"+this.salon+" </div>").remove;
+				td.append("<div data-id='saloncito' class='" +this.salon+ "'>"+this.salon+" "+this.cantidad_alumnos+" </div>").remove;
 				console.log(td);
 				});				
 				console.log(response);
